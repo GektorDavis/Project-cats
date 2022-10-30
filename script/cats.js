fa6f2a5248571cef6ba7df166eb10fb61ea16dc2
Алёна Грун <kav2016@mail.ru>
2022-10-30 17:39:04 +0500

    Project cats

---
+++ script/cats.js
@@ -0,0 +1,91 @@
+const cats = [
+  {
+    id: 41,
+    name: 'Честер',
+    favourite: true,
+    rate: 10,
+    age: 8,
+    description: 'Деловой',
+    img_link:
+      'https://icdn.lenta.ru/images/2021/12/13/00/20211213002603327/square_320_6df6f5217f0f0b3e856b6a0276c1d270.jpg',
+  },
+  {
+    id: 42,
+    name: 'Колбасик',
+    favourite: false,
+    rate: 3,
+    age: 4,
+    description: 'Любитель колбасы',
+    img_link:
+      'https://sun9-67.userapi.com/impg/a_CgUIPoM7fFT86LFGT_qHzD2QpzkzjhJ3qhwQ/0qwKTuQuI8g.jpg?size=488x604&quality=96&sign=74bbcb606ac73d8c495fc6a2b123cce8&type=album',
+  },
+  {
+    id: 43,
+    name: 'Персик',
+    favourite: false,
+    rate: 5,
+    age: 6,
+    description: 'Изумительный цвет',
+    img_link:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAQceVn4fRjNX5l9xZxbr3c-kYcqn40ZPWqw&usqp=CAU',
+  },
+  {
+    id: 44,
+    name: 'Коксик',
+    favourite: false,
+    rate: 4,
+    age: 3,
+    description: 'Почти жидкость',
+    img_link: 'https://www.ridus.ru/images/2018/3/27/740402/hd_dfb23d5050.jpg',
+  },
+  {
+    id: 45,
+    name: 'Тимоха',
+    favourite: false,
+    rate: 4,
+    age: 6,
+    description: 'Временно безработный',
+    img_link:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBD3ED7nbFUuVgeNVg2lg9vGwCwYSL7cKXLQ&usqp=CAU',
+  },
+  {
+    id: 46,
+    name: 'Мурзик',
+    favourite: false,
+    rate: 7,
+    age: 7,
+    description: 'Любитель пошалить',
+    img_link:
+      'https://www.purina.ru/sites/default/files/2021-07/%D0%92%D1%8F%D0%B7%D0%BA%D0%B0%20%D0%BA%D0%BE%D1%82%D0%BE%D0%B2%20opengraph_0.jpg',
+  },
+  {
+    id: 47,
+    name: 'Кузя',
+    favourite: false,
+    rate: 8,
+    age: 5,
+    description: 'Меланхолик',
+    img_link:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMKPjSbMzhQY3EWuPAfzGXJQ27jj688WCOTw&usqp=CAU',
+  },
+  {
+    id: 48,
+    name: 'Буся',
+    favourite: false,
+    rate: 8,
+    age: 6,
+    description: 'Обожает музеи',
+    img_link:
+      'https://upload.wikimedia.org/wikipedia/commons/e/e3/Hermitage_cat.jpeg',
+  },
+  {
+    id: 49,
+    name: 'Брунгильда',
+    favourite: false,
+    rate: 8,
+    age: 4,
+    description: 'Ласковая',
+    img_link:
+      'https://zoomagazin.dp.ua/image/catalog/blog-img/koshka-murchit.jpg',
+  },
+];
